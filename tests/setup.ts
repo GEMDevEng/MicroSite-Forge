0fbecc53eded9605f5eb38ccde73b2948ccdf7b8
GEMDevEng <gemdev25@gmail.com>
2025-10-03 22:08:42 +0200

    test: safer Response.json polyfill typing to avoid explicit any

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,6 +1,12 @@
 // Polyfill Response.json for Node test environment
-if (!(Response as any).json) {
-  ;(Response as any).json = (data: unknown, init?: ResponseInit) => {
+declare global {
+  interface Response {
+    json?: (data: unknown, init?: ResponseInit) => Response
+  }
+}
+
+if (!(Response as unknown as typeof globalThis & { json?: unknown }).json) {
+  ;(Response as unknown as any).json = (data: unknown, init?: ResponseInit) => {
     return new Response(JSON.stringify(data), {
       ...init,
       headers: { 'Content-Type': 'application/json', ...(init?.headers || {}) },
